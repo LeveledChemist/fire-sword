3e26fbc70f55ccf13278b197f8e96262e52fa77b
LevelChem <88857473+LeveledChemist@users.noreply.github.com>
2023-09-19 10:00:26 -0500

    First push to include code that reflects my username in a join event

--- src/main/java/com/levelchemistry/firesword/Fire_sword.java
+++ src/main/java/com/levelchemistry/firesword/Fire_sword.java
@@ -1,10 +1,16 @@
 package com.levelchemistry.firesword;
 
 import io.papermc.paper.event.block.BlockBreakBlockEvent;
+import org.bukkit.Material;
+import org.bukkit.Server;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -32,5 +38,18 @@ public final class Fire_sword extends JavaPlugin implements Listener {
         String playerName;
         playerName = String.valueOf(event.getPlayer().getName());
         if (Objects.equals(playerName, "LevelChemistry")) event.setJoinMessage("Welcome Level Chemistry!");
+        if (Objects.equals(playerName, "vx8")) event.setJoinMessage("Hahahaha its vx8");
     }
+
+    //The code below ideally will send a chat message to the player letting them know they dropped an item
+    public void onDiamondDrop(PlayerDropItemEvent event, Player entity) {
+        event.getItemDrop();
+        entity.sendMessage("You have dropped an item");
+        return;
+    }
+    //Filler code until I can get the above working
+    //public void chatDiamondDrop(Player entity) {
+
+        //entity.sendRawMessage("You have dropped an item");
+    //}
 }
